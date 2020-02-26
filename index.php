d1dd054872be32fb50f3b7b228c17e223d867783
WilliamWallitt <willcdswallitt@gmail.com>
2020-02-26 19:53:43 +0000

    added redirect by viewport size - Olly

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      if (window.innerWidth < 960) {
+        window.location.href = "views/DepartmentPage.php";
+      }
+
+      if (window.innerWidth >= 960) {
+        window.location.href = "views/LoginPage.php";
+      }
+    </script>
+  </body>
+</html>
