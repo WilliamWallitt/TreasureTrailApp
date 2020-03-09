85c64934b89342daac2a5be88fb15202fc180e78
WilliamWallitt <willcdswallitt@gmail.com>
2020-03-09 15:58:05 +0000

    popup for pirate narrative Will/Stefon

--- views/DepartmentPage.php
+++ views/DepartmentPage.php
@@ -3,115 +3,50 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400' rel='stylesheet'>
-  <script
-    src="https://use.fontawesome.com/releases/v5.12.1/js/all.js"
-    data-search-pseudo-elements>
-  </script>
-  <script src = "../audio/howler.js"></script>
-  <script>
-    var waves = new Howl({
-      src: ['../audio/waves.mp3'],
-      loop: true,
-      volume: 0.05,
-    });
-    var click_sound = new Howl({
-      src: ['../audio/button-click.mp3']
-    });
-
-    function button_click()
-    {
-        click_sound.play();
-    }
-  </script>
-  <link rel="stylesheet" type="text/css" href='../public/stylesheets/departmentpage.css'>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../public/stylesheets/departmentpage.css">
+<!------ Include the above in your HEAD tag ---------->
 </head>
-<body style="background: url('../public/img/departmentBackground.jpg') no-repeat center fixed; background-size: cover;">
-<!-- ;==========================================
-; Title:  Front end Department Page - HTML
-; Author: William Wallitt, Edward Soutar, Bevan Roberts
-; Date:   25 Feb 2020
-;========================================== -->
+<body>
 
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow">
-    <div class="container justify-content-center">
-      <div class="container p-1 text-center">
-        <!-- this is our own animation container -->
-        <div id=container>
-          <div id=flip>
-            <!-- flipping between each div  -->
-            <div><div>Choose A Department</h1></div></div>
-            <div><div>Choose A Department</h1></div></div>
-            <div><div>Choose A Department</h1></div></div>
-          </div>
-        </div>
-      </div>
-      </div>
+<video autoplay muted loop id="myVideo">
+  <source src="../public/img/stormySeas.mp4" type="video/mp4">
+</video>
+
+<div class="box" style="display: none;">
+	<a class="button" id="pirategif" href="#popup1">Let me Pop up</a>
+</div>
+
+
+<div id="popup1" class="overlay">
+	<div class="popup">
+		<h2><img id="pirate"src="../public/img/talking.gif"></h2>
+		<a class="close" href="#">&times;</a>
+		<!-- <div class="content"> -->
+    <div class="content">
+      <h1 class='lead' id="textsize">
+        “Ay freshers, I need your help! I’ve lost me treasure all around the campus. I’ve got me treasure map marked out, but I need help getting it back. Been spotting scavengers around these parts recently and the longer I take, the more of me treasure they get. I’ve protected me treasure at each location behind some questions, but in me old age I've forgotten them! Help an old pirate out and help me answer these questions. Time is of the essence, let's get started!"
+      </h1>
     </div>
-  </nav>
+	</div>
+</div>
 
 
-<!-- adding border to our list and making sure its centered -->
-  <div class="container list-group p-0 border border-dark rounded-lg">
-    <!-- search bar - want it as a form-group -->
-    <a class="list-group-item">
-        <form class="form-inline md-form form-sm mt-0">
-            <i class="fas fa-search" aria-hidden="true"></i>
-            <input class="form-control form-control-sm ml-3 w-75" id="myInput" onkeyup="myFunction()" type="text" placeholder="Search"
-            aria-label="Search">
-        </form>
-    </a>
 
-    <!-- list of our departments -->
-    <ul class="list-group text-center" id="myUL">
 
-    </ul>
-  </div>
-</script>
+
+
 <script>
-// getting the departments from the DB -> setting them as <Li></Li> elements
-fetch("../app/get_departments.php").then(response => {
-    return response.json();
-}).then(data => {
-  for (i = 0; i < data.length; i++) {
-    $("#myUL").append("<li class=\"list-group-item\"><a href=\"../views/cluepage.php?id=" + data[i].department_id + "\" id=\"btn\" onclick=button_click()>" + data[i].department_name + "<i class=\"fas fa-arrow-right\"></i></a></li>");
-  }
-}).catch(err => {
-    // catch err
-    console.log(err);
-});
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  // get input field
-  input = document.getElementById('myInput');
-  // get input value
-  filter = input.value.toUpperCase();
-  // get ul (unordered list)
-  ul = document.getElementById("myUL");
-  // get all li elements
-  li = ul.getElementsByTagName('li');
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    // get first tag's contents
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        // display that li
-      li[i].style.display = "";
-    } else {
-        // dont display that li
-      li[i].style.display = "none";
-    }
-  }
-}
+
+  document.getElementById("pirategif").click();
+
+
 </script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.1.0.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
+    </body>
 </html>
