ad1d0f30fef143760092b9eaf02541edfb007eab
WilliamWallitt <willcdswallitt@gmail.com>
2020-03-11 23:13:13 +0000

    bug fix

--- views/faqPage.php
+++ views/faqPage.php
@@ -12,23 +12,21 @@
 
 <!-- ;==========================================
 ; Title:  Front end FAQ Page - HTML
-; Author: William Wallitt, Justin Van Daalen, Oliver Fawcett
+; Author: William Wallitt, Justin Van Daalen
 ; Date:   25 Feb 2020
 ;========================================== -->
 
 <!-- setting the background -->
 <body style="background: url('../public/img/Backgroundnew.jpeg') no-repeat center fixed; background-size: cover;">
 
-
-
-    <button class="btn btn-outline-light" style="margin: 0 auto; width: 100vw">Back to game</button> 
+    <button class="btn btn-outline-light mt-2 p-2" onclick="window.location='../views/cluePage.php';" style="margin: 0 auto; width: 100vw";>Back to game</button> 
 
     <h2 class="text-white">Frequently Asked Questions</h2>
 
     <!-- using svg-import -->
     <div style="visibility: hidden; position: absolute; width: 0px; height: 0px;">
       <svg xmlns="http://www.w3.org/2000/svg">
-        <symbol viewBox="0 0 24 24" id="expand-more">
+        <symbol viewBox="0 0 20 20" id="expand-more">
           <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/><path d="M0 0h24v24H0z" fill="none"/>
         </symbol>
         <symbol viewBox="0 0 24 24" id="close">
@@ -46,9 +44,15 @@
         <div class="container-xl px-3 px-md-6">
           <div class="gutter gutter-xl-spacious d-md-flex">
             <div class="col-12 col-md-5 text-white">
-              <h4 class="h2">Can't find what you're looking for?</h4>
+              <h4 class="h2">Need help? Contact a GameKeeper!</h4>
               <!-- gamekeeper email? -->
-              <a class="btn btn-outline-dark m-2 text-white border-light" href="mailto:willcdswallitt@gmail.com?Subject=User%20Help%20Request">Contact a GameKeeper</a>
+              <form>
+                <div class="form-group">
+                  <a class="btn btn-outline-dark m-2 text-white border-light" href="mailto:willcdswallitt@gmail.com?Subject=Help">Contact a GameKeeper</a> 
+                </div>
+              </form>
+
+              <h1 class="lead">In the event of an emergency, where you or a member of your group requires urgent medical assistance you must call 999.</h1>
             </div>
           </div>
         </div>
@@ -58,6 +62,23 @@
 
 <!-- front end javascript code -->
 <script src="../Javascript/faqPage.js"></script>
+
+
+<script>
+
+function emailGamekeeper() {
+
+  let emailaddress = $("#emailAddress").text();
+  let message = $("#message").text();
+
+
+
+
+
+}
+
+
+</script>
 
 <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
