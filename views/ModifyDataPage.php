eaa31f2a0cc5527d898e0d9231d5deb3fa53ee28
edwardsoutar <46631661+edwardsoutar@users.noreply.github.com>
2020-03-11 19:36:06 +0000

    Update ModifyDataPage.php

--- views/ModifyDataPage.php
+++ views/ModifyDataPage.php
@@ -17,6 +17,12 @@ if (!isset($_SESSION['username'])) {
     src="https://use.fontawesome.com/releases/v5.12.1/js/all.js"
     data-search-pseudo-elements></script>
   <link rel="stylesheet" type="text/css" href='../public/stylesheets/modifyPage.css'>
+  <script src="../Javascript/ModifyDataPage_Buildings.js"></script>
+  <script src="../Javascript/ModifyDataPage_Clues.js"></script>
+  <script src="../Javascript/ModifyDataPage_Departments.js"></script>
+  <script src="../Javascript/ModifyDataPage_FAQ.js"></script>
+  <script src="../Javascript/ModifyDataPage_Routes.js"></script>
+  <script src="../Javascript/ModifyDataPage_GroupTracking.js"></script>
 </head>
 
 <!-- ;==========================================
@@ -29,7 +35,7 @@ if (!isset($_SESSION['username'])) {
 .menu a {
   display: block;
   text-decoration: none;
-}  
+}
 
 </style>
 
@@ -82,13 +88,13 @@ if (!isset($_SESSION['username'])) {
             <tbody id="departments">
             </tbody>
           </table>
-        </div>        
+        </div>
       </div>
       <div class="tab-pane" id="Clues">
         <!-- clue content -->
         <div class="container text-center">
           <h1 class="lead">Add a new clue!</h1>
-        
+
         </div>
 
         <form>
@@ -229,10 +235,10 @@ if (!isset($_SESSION['username'])) {
               </tr>
             </thead>
             <tbody id="buildings">
-              
+
             </tbody>
           </table>
-        </div>        
+        </div>
       </div>
 
       <!-- Routes content -->
@@ -248,7 +254,7 @@ if (!isset($_SESSION['username'])) {
           <div class="row m-3">
             <div class="col">
               <select class="custom-select my-1 mr-sm-2" id="departmentdropdown">
-                <option selected>Department</option>         
+                <option selected>Department</option>
               </select>
             </div>
             <div class="col">
@@ -274,7 +280,7 @@ if (!isset($_SESSION['username'])) {
 
             </tbody>
           </table>
-        </div> 
+        </div>
       </div>
 
       <!-- FAQ content -->
@@ -311,7 +317,7 @@ if (!isset($_SESSION['username'])) {
 
             </tbody>
           </table>
-        </div> 
+        </div>
       </div>
 
 
@@ -332,511 +338,18 @@ if (!isset($_SESSION['username'])) {
             <tbody id="grouptracking">
             </tbody>
           </table>
-        </div>        
+        </div>
       </div>
-
-
-
-
-
-
-
     </div><!-- tab content -->
-
   </div>
-
   </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <!-- Icons -->
-    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <script>
-      feather.replace()
-    </script>
-
-<script>
-
-
-  // ;==========================================
-  // ; Title:  Front end Javascript request's (get/posts requests)
-  // ; Author: William Wallitt, Justin Van Daalen, Stephan Kubal, Oliver Fawcett
-  // ; Date:   25 Feb 2020
-  // ;==========================================
-
-
-  function addFAQ() {
-
-    let question = document.getElementById("question").value;
-    let answer = document.getElementById("answer").value;
-
-    if (question.length == 0 || answer.length == 0) {
-      alert("Please fill in the required fields");
-      return;
-    } 
-    fetch('../app/create_faq.php', {
-      headers: { "Content-Type": "application/json; charset=utf-8" },
-      method: 'POST',
-      body: JSON.stringify({
-        question: question,
-        answer: answer,
-      })
-    }).then(response => {
-      return response.json();
-    }).then(data => {
-      document.getElementById("question").value = ""; 
-      document.getElementById("answer").value = "";
-      fetchFAQs();
-    });
-  }
-
-  function deleteFAQ(faq_id) {
-
-    fetch("../app/remove_faq.php?faq_id=" + faq_id).then(response => {
-        return response.json();
-    }).then(data => {
-      fetchFAQs();
-    });
-
-  }
-
-function addDepartment() {
-
-  let departmentName = document.getElementById("departmentName").value;
-
-  if (departmentName.length == 0) {
-    alert("Please fill in the required fileds");
-    return;
-  } 
-  fetch('../app/create_department.php', {
-    headers: { "Content-Type": "application/json; charset=utf-8" },
-    method: 'POST',
-    body: JSON.stringify({
-      department_name: departmentName,
-    })
-  }).then(response => {
-    return response.json();
-  }).then(data => {
-    document.getElementById("departmentName").value = ""; 
-    fetchDepartments();
-  });
-}
-
-
-function deleteDepartment(department_id) {
-
-  fetch("../app/remove_department.php?department_id=" + department_id).then(response => {
-      return response.json();
-  }).then(data => {
-    fetchDepartments();
-  });
-}
-
-
-function addBuilding() {
-
-  let buildingName = document.getElementById("buildingname").value;
-  let lat = document.getElementById("latcoord").value;
-  let lng = document.getElementById("lngcoord").value;
-  let extraInfo = document.getElementById("extrainfo").value;
-  let narrative = document.getElementById("narrative").value;
-
-  if (buildingName.length == 0 || lat.length == 0 || lng.length == 0 || extraInfo.length == 0 || narrative.length == 0) {
-    alert("Please fill in the required fields - ");
-    return;
-  } 
-
-  fetch('../app/create_building.php', {
-      headers: { "Content-Type": "application/json; charset=utf-8" },
-      method: 'POST',
-      body: JSON.stringify({
-        building_name: buildingName,
-        latitude: lat,
-        longitude: lng,
-        extra_info: extraInfo,
-        narrative: narrative,
-        clues: []
-      })
-    }).then(response => {
-      return response.json();
-    }).then(data => {
-      document.getElementById("buildingname").value = ""; 
-      document.getElementById("latcoord").value = ""; 
-      document.getElementById("lngcoord").value = ""; 
-      document.getElementById("extrainfo").value = ""; 
-      document.getElementById("narrative").value = "";
-      fetchBuildings();
-
-    });
-}
-
-
-  function deleteBuilding(building_id) {
-
-    fetch("../app/remove_building.php?building_id=" + building_id).then(response => {
-        return response.json();
-    }).then(data => {
-      fetchBuildings();
-    });
-  }
-
-  function addRoute() {
-
-    let departments = document.getElementById("departmentdropdown");
-    let department_id = departments.options[departments.selectedIndex].id;
-    
-    let buildings = document.getElementById("buildingdropdown");
-    let building_id = buildings.options[buildings.selectedIndex].id;
-
-    fetch('../app/create_route.php', {
-      headers: { "Content-Type": "application/json; charset=utf-8" },
-      method: 'POST',
-      body: JSON.stringify({
-        department_id: department_id,
-        building_id: building_id
-      })
-    }).then(response => {
-      return response.json();
-    }).then(data => {
-      departments.selectedIndex = 0;
-      buildings.selectedIndex = 0;
-      fetchRoute();
-    });
-  }
-
-
-  function deleteRoute(route_id) {
-
-    fetch("../app/remove_route.php?route_id=" + route_id).then(response => {
-        return response.json();
-    }).then(data => {
-      fetchRoute();
-    }); 
-  } 
-
-
-  // ;==========================================
-  // ; Title:  Front end Javascript request's (add/delete requests)
-  // ; Author: William Wallitt, Justin Van Daalen, Oliver Fawcett
-  // ; Date:   25 Feb 2020
-  // ;==========================================
-
-
-function fetchGroupTracking() {
-
-  fetch("../app/get_all_tracking.php").then(response => {
-      return response.json();
-  }).then(data => {
-
-    const groupTracking = document.getElementById("grouptracking");
-    let groupTrackingHTML = "";
-    for (let index = 0; index < data.length; index++) {
-      const team_name = data[index].team_name;
-      const department = data[index].department.department_name;
-      const currentBuilding = data[index].current_building.building_name;
-      const score = data[index].score;
-      groupTrackingHTML += "<tr><td>" + team_name + "</td><td>" + department + "</td><td>" + currentBuilding + "</td><td>" + score + "</td></tr>";
-      
-    }
-    groupTracking.innerHTML = groupTrackingHTML;
-  }).catch(err => {
-      console.log(err);
-  });
-
-}
-
-
-
-
-
-
-
-
-
-
-
-  function fetchDepartments() {
-
-    fetch("../app/get_departments.php").then(response => {
-        return response.json();
-    }).then(data => {
-      const departmentTable = document.getElementById("departments");
-      var html = "";
-
-      for (let index = 0; index < data.length; index++) {
-
-        const department = data[index].department_name;
-        html += "<tr><td>" + department + "</td><td><button class=\"btn btn-sm btn-outline-danger\" onclick=\"deleteDepartment("+ data[index].department_id + ")\">Delete</button></td></tr>";
-      
-      }
-
-      departmentTable.innerHTML = html;
-
-    }).catch(err => {
-        console.log(err);
-    });
-  }
-
-  function addClue() {
-
-    let buildings = document.getElementById("cluebuildingdropdown");
-    let building_id = buildings.options[buildings.selectedIndex].id;
-
-    let clueName = document.getElementById("clueName").value;
-
-    let answer1 = document.getElementById("question1").value;
-    let answer1correct = document.getElementsByName("true1")[0].checked;
-
-    let answer2 = document.getElementById("question2").value;
-    let answer2correct = document.getElementsByName("true2")[0].checked;
-    
-    let answer3 = document.getElementById("question3").value;
-    let answer3correct = document.getElementsByName("true3")[0].checked;
-
-    if (clueName.length == 0 || (!answer1correct && !answer2correct && !answer3correct)) {
-      alert("Please fill in the required fields - ");
-      return;
-    } 
-
-    fetch('../app/create_clue.php', {
-      headers: { "Content-Type": "application/json; charset=utf-8" },
-      method: 'POST',
-      body: JSON.stringify({
-        building_id: building_id,
-        clue: clueName,
-        answers: [
-          {
-            answer: answer1,
-            correct: answer1correct
-          },
-          {
-            answer: answer2,
-            correct: answer2correct
-          },
-          {
-            answer: answer2,
-            correct: answer2correct
-          }
-        ]
-      })
-    }).then(response => {
-      return response.json();
-    }).then(data => {
-      buildings.selectedIndex = 0;
-      document.getElementById("buildingname").value = ""; 
-      document.getElementById("clueName").value = ""; 
-      document.getElementById("question1").value = ""; 
-      document.getElementById("question2").value = ""; 
-      document.getElementById("question3").value = ""; 
-      document.getElementsByName("true1")[0].checked = true;
-      document.getElementsByName("true2")[0].checked = false;
-      document.getElementsByName("true3")[0].checked = false;
-      fetchClues();
-    });
-  }
-
-  function deleteClue(clue_id) {
-
-    fetch("../app/remove_clue.php?clue_id=" + clue_id).then(response => {
-        return response.json();
-    }).then(data => {
-      fetchClues();
-    }); 
-  } 
-
-  function deleteAnswer(answer_id) {
-
-    fetch("../app/remove_answer.php?answer_id=" + answer_id).then(response => {
-        return response.json();
-    }).then(data => {
-      fetchClues();
-    }); 
-  } 
-
-  function fetchClues() {
-
-
-    fetch("../app/get_all_clues.php").then(response => {
-        return response.json();
-    }).then(data => {
-      const cluesTable = document.getElementById("clues");
-
-      let html = "";
-      for (let index = 0; index < data.length; index++) {
-        const clue_id = data[index].clue_id;
-        const clue = data[index].clue;
-        html += "<tr> <td>" + clue + "</td><td> <table class=\"table table-hover table-light bg-light table-sm\"> <thead> <tr> <th>#</th> <th>Answer</th> <th>Correct</th> </tr></thead> <tbody>";
-        
-        for (let index2 = 0; index2 < data[index].answers.length; index2++) {
-          const answer_id = data[index].answers[index2].answer_id;
-          const answer = data[index].answers[index2].answer;
-          const correct = data[index].answers[index2].correct;
-
-          html += "<tr> <th scope=\"row\">" + (index2 + 1) + "</th> <td>" + answer + "</td><td>" + correct + "</td><td><button class=\"btn btn-sm btn-outline-danger\" onclick=\"deleteAnswer("+ answer_id + ")\"><i class=\"fas fa-minus\"></i></button></td></tr>";
-        } 
-        html += "</tbody> </table> </td><td class=\"text-center\"><button class=\"btn btn-sm btn-outline-danger mt-5\" onclick=\"deleteClue("+ clue_id + ")\">Delete</button></td></tr>";     
-      }
-
-      cluesTable.innerHTML = html;
-
-      populateClueBuildings();
-    }).catch(err => {
-        console.log(err);
-    });
-  }
-
-  function populateClueBuildings() {
-    fetch("../app/get_all_buildings.php").then(response => {
-        return response.json();
-    }).then(data => {
-      const buildingDropDown = document.getElementById("cluebuildingdropdown");
-      var html = "";
-      for (let index = 0; index < data.length; index++) {
-        let building = data[index];
-        html += "<option id=\"" + building.building_id + "\">" + building.building_name + "</option>"
-      }
-      buildingDropDown.innerHTML = html;
-    }).catch(err => {
-        // catch err
-        console.log(err);
-    });
-  }
-
-  function fetchBuildings() {
-
-    fetch("../app/get_all_buildings.php").then(response => {
-        return response.json();
-    }).then(data => {
-
-      console.log(data);
-      const departmentTable = document.getElementById("buildings");
-      let departmentTableHTML = "";
-      for (let index = 0; index < data.length; index++) {
-        const building_id = data[index].building_id;
-        const buildingName = data[index].building_name;
-        const lat = data[index].latitude;
-        const lng = data[index].longitude;
-        const extraInfo = data[index].extra_info;
-        const narrative = data[index].narrative;
-        departmentTableHTML += "<tr><td>" + buildingName + "</td><td>" + lat + "</td><td>" + lng + "</td><td>" + extraInfo + "</td><td><img src=\"https://api.qrserver.com/v1/create-qr-code/?size=50x50&data=" +  building_id + "\"></img></td><td>" + narrative +"</td><td><button class=\"btn btn-sm btn-outline-danger\" onclick=\"deleteBuilding("+ data[index].building_id + ")\">Delete</button></td></tr>";
-        
-      }
-
-      departmentTable.innerHTML = departmentTableHTML;
-    }).catch(err => {
-        console.log(err);
-    });
-
-
-  }
-
-
-  function fetchRoute() {
-
-    // due to only partial data being load (buildings and departments) as not all of them had a route
-    // we need to fetch bot get_all_departments and get_all_buildings for the complete list
-    
-
-    // get all departments
-    fetch("../app/get_departments.php").then(response => {
-        return response.json();
-    }).then(data => {
-      // get the drop down list element
-      const departmentDropDown = document.getElementById("departmentdropdown");
-      var html1 = "";
-      for (let index = 0; index < data.length; index++) {
-        // iterate over all the departments and add them to our string
-        let department = data[index];
-        html1 += "<option id=\"" + department.department_id + "\">" + department.department_name + "</option>"
-        
-      }
-      // set the department drop down innnerHTML to our string
-      departmentDropDown.innerHTML = html1;
-
-    }).catch(err => {
-        console.log(err);
-    });
-
-    // get all buildings
-    fetch("../app/get_all_buildings.php").then(response => {
-        return response.json();
-    }).then(data => {
-      // get building drop down list
-      const buildingDropDown = document.getElementById("buildingdropdown");
-
-      var html = "";
-      for (let index = 0; index < data.length; index++) {
-        let building = data[index];
-        html += "<option id=\"" + building.building_id + "\">" + building.building_name + "</option>"
-      }
-
-      buildingDropDown.innerHTML = html;
-    }).catch(err => {
-        console.log(err);
-    });
-
-    // fetching all the routes and populating the route table
-    fetch("../app/get_all_routes.php").then(response => {
-        return response.json();
-    }).then(data => {
-
-      const departmentTable = document.getElementById("routes");
-      let departmentTableHTML = "";
-      for (let index = 0; index < data.length; index++) {
-
-        const department = data[index].department_name;
-
-        for (let index2 = 0; index2 < data[index].buildings.length; index2++) {
-          // each department has an array of buildings - so we loop through them and add each to our list
-          const building = data[index].buildings[index2].building_name;
-
-          var dep = "<tr><td>" + department + "</td><td>"+ building + "</td><td><button class=\"btn btn-sm btn-outline-danger\" onclick=\"deleteRoute("+ data[index].buildings[index2].route_id + ")\">Delete</button></td></tr>";
-          var build = "<tr><td></td><td>"+ building + "</td><td><button class=\"btn btn-sm btn-outline-danger\" onclick=\"deleteRoute("+ data[index].buildings[index2].route_id + ")\">Delete</button></td></tr>";
-
-          if (index2 == 0) {
-            // so we only have one department for each list of buildings for the route (makes it look nicer)
-            departmentTableHTML += dep;
-          } else {
-            departmentTableHTML += build;
-          }
-        }
-                
-      }
-
-      // set our element's innerHTML to our variable
-      departmentTable.innerHTML = departmentTableHTML;
-
-    }).catch(err => {
-        console.log(err);
-    });
-
-
-  }
-
-  function fetchFAQs() {
-    fetch("../app/get_faqs.php").then(response => {
-        return response.json();
-    }).then(data => {
-      const faqTable = document.getElementById("faq");
-      let faqTableHTML = "";
-      for (let index = 0; index < data.length; index++) {
-        const question = data[index].question;
-        const answer = data[index].answer;
-        faqTableHTML += "<tr><td>" + question + "</td><td>"+ answer + "</td><td><button class=\"btn btn-sm btn-outline-danger\" onclick=\"deleteFAQ("+ data[index].faq_id + ")\">Delete</button></td></tr>";
-        
-      } 
-      faqTable.innerHTML = faqTableHTML;
-    }).catch(err => {
-        console.log(err);
-    });
-
-
-  } 
-
-  // getting departments -> as its the first page
-  fetchDepartments();
-
-</script>
+  <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+  <script>
+    feather.replace()
+  </script>
 </body>
-
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -844,4 +357,3 @@ function fetchGroupTracking() {
 
 </body>
 </html>
-
