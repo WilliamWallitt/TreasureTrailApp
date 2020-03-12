5d58c94f621e5c84c8f941fe350578b98a9b1e04
edwardsoutar <46631661+edwardsoutar@users.noreply.github.com>
2020-03-12 04:54:26 +0000

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,3 +1,9 @@
+/* ;==========================================
+; Title:  Back End User Test
+; Author: Justin Van Daalen
+; Date:   12 Mar 2020
+;==========================================  */
+
 <?php
 use PHPUnit\Framework\TestCase;
 
