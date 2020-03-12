c131439f4f7bbba7811ccc6d16bd5f11205e133f
edwardsoutar <46631661+edwardsoutar@users.noreply.github.com>
2020-03-12 04:39:41 +0000

    Update finishedPageAnimatons.js

--- Javascript/finishedPageAnimatons.js
+++ Javascript/finishedPageAnimatons.js
@@ -1,7 +1,7 @@
 // ;==========================================
 // ; Title:  Front end Javascript request's (Finished Page - Animations)
 // ; Author: William Wallitt, Justin Van Daalen, Stephan Kubal, Oliver Fawcett
-// ; Date:   25 Feb 2020
+// ; Date:   12 Mar 2020
 // ;==========================================
 
 // animations
