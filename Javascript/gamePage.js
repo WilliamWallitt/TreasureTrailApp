fb6d6740c60643118c401ca982154b6c163753f7
edwardsoutar <46631661+edwardsoutar@users.noreply.github.com>
2020-03-12 04:37:20 +0000

    Update gamePage.js

--- Javascript/gamePage.js
+++ Javascript/gamePage.js
@@ -1,3 +1,9 @@
+// ;==========================================
+// ; Title:  Front end Javascript request's (Game Page)
+// ; Author: William Wallitt, Justin Van Daalen, Stephan Kubal, Oliver Fawcett
+// ; Date:   25 Feb 2020
+// ;==========================================
+
 function onDepartmentClick(department_id) {
 
     fetch('../app/create_user.php', {
